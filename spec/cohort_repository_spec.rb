b8425df89fe5d6d74f5dc9156a1a2a292da4b37c
Thomas McCoy <finnmccoy99@gmail.com>
2023-01-06 11:15:04 +0000

    Implemented first test for #find_with_students method

---
+++ spec/cohort_repository_spec.rb
@@ -0,0 +1,24 @@
+require "cohort_repository"
+require "cohort"
+require "student"
+
+describe CohortRepository do
+
+  before(:each) do
+    @repo = CohortRepository.new
+    @cohort = Cohort.new
+    @cohort.name = "Second" ; @cohort.starting_date = "2020-11-30"
+    @cohort.id = 2
+    @student1 = Student.new ; @student2 = Student.new
+    @student1.name = "Jeff" ; @student2.name = "Mark"
+    @student1.id = 3 ; @student2.id = 4
+    @student1.cohort_id = 2 ; @student2.cohort_id = 2
+  end
+
+  context "#find_with_students given integer argument" do
+    it "#returns cohort with corresponding id" do
+      expect(@repo.find_with_students(2)).to eq @cohort
+    end
+  end
+
+end
